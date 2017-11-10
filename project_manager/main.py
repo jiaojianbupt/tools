0c0fc702c866d256cf5f54004be047df1f475b12
jiaojian <jiaojian@bytedance.com>
2017-11-10 17:27:55 +0800

    interrupt

--- project_manager/main.py
+++ project_manager/main.py
@@ -8,4 +8,7 @@ import sys
 if __name__ == '__main__':
     sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
     from manager import manage
-    manage()
+    try:
+        manage()
+    except KeyboardInterrupt:
+        print 'Aborted.'
