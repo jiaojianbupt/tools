47cc8fca8285c4f10152b7c31f8fc6d465889bb3
jiaojian <jiaojian@bytedance.com>
2018-06-25 18:35:45 +0800

    fix alias on zsh

--- project_manager/initialize.sh
+++ project_manager/initialize.sh
@@ -4,11 +4,19 @@ update_repos(){
      echo `${PROJECT_MANAGER_PATH}/default_generate.sh $1 $2`
 }
 source ${PROJECT_MANAGER_PATH}/conf.local
-alias usp=`update_repos ${VA_DEV} py`
-alias uspcn=`update_repos ${CN_DEV} py`
-alias uspsg=`update_repos ${SG_DEV} py`
-alias uspaliva=`update_repos ${ALIVA_DEV} py`
-alias usg=`update_repos ${VA_DEV} go`
-alias usgcn=`update_repos ${CN_DEV} go`
-alias usgsg=`update_repos ${SG_DEV} go`
-alias usgaliva=`update_repos ${ALIVA_DEV} go`
+cmd_tmp=`update_repos ${VA_DEV} py`
+alias usp=${cmd_tmp}
+cmd_tmp=`update_repos ${CN_DEV} py`
+alias uspcn=${cmd_tmp}
+cmd_tmp=`update_repos ${SG_DEV} py`
+alias uspsg=${cmd_tmp}
+cmd_tmp=`update_repos ${ALIVA_DEV} py`
+alias uspaliva=${cmd_tmp}
+cmd_tmp=`update_repos ${VA_DEV} go`
+alias usg=${cmd_tmp}
+cmd_tmp=`update_repos ${CN_DEV} go`
+alias usgcn=${cmd_tmp}
+cmd_tmp=`update_repos ${SG_DEV} go`
+alias usgsg=${cmd_tmp}
+cmd_tmp=`update_repos ${ALIVA_DEV} go`
+alias usgaliva=${cmd_tmp}
